031d445ec033bfe2c773a736c032cf8e7dcf30dc
Derek Groen <djgroennl@gmail.com>
2016-08-03 10:54:40 +0100

    Added some dry code

--- Python-scripts/1238073-numpy-and-matplotlib.py
+++ Python-scripts/1238073-numpy-and-matplotlib.py
@@ -17,7 +17,10 @@ with open ('D:/Python/In/ID-and-average-pressure/step_and_pressure.txt','r') as 
     plots = csv.reader(csvfile)
     for row in plots:
         xy = np.vstack([xy,[float(row[0]), float(row[1])]])
+        #xy = np.vstack([xy,[float(row[0]), float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7])]])
 print (xy)
+
+#magnitudes = np.sqrt(xy[:,4]*xy[:,4] + xy[:,5]*xy[:,5] + xy[:,6]*xy[:,6])
 
 plt.plot(xy[:,0], xy[:,1], label='blood flow')
 
